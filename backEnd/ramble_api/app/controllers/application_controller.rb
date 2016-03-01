e15095299ee89964d6fa4bdb93516939f314bdc6
cc2RG <rbgalloway88@gmail.com>
2016-03-01 10:05:00 +0000

    db setup

--- backEnd/ramble_api/app/controllers/application_controller.rb
+++ backEnd/ramble_api/app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+include ActionController::Serialization
+
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
