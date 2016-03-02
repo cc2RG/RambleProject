6f2618b14474b8ade62336afb1ba242b789dabd6
cc2RG <rbgalloway88@gmail.com>
2016-03-02 14:26:59 +0000

    rails-api is right format, adds body-parser to node modules

--- backEnd/ramble_api/config/application.rb
+++ backEnd/ramble_api/config/application.rb
@@ -8,6 +8,20 @@ Bundler.require(*Rails.groups)
 
 module RambleApi
   class Application < Rails::Application
+    config.api_only = false
+    config.active_record.raise_in_transactional_callbacks = true
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
+
+  end
+
+
+
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -21,6 +35,6 @@ module RambleApi
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    
   end
-end
+
